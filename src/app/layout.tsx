683ec9413026b764049f0934b08d76c7e298781c
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-10-12 00:20:52 +0530

    fix:reduce the impact of third-party code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,15 +58,17 @@ export default function RootLayout({
     <html lang="en" className="relative">
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-          `}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
       </Script>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
