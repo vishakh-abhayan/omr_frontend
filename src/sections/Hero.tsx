4cc4eb87f3b5510b209f6bad6b893ac15004072f
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-10-11 23:59:57 +0530

    feat:hero content and image optimisation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,44 +1,21 @@
-"use client";
-
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.webp";
-
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useMotionValueEvent,
-} from "framer-motion";
-import { useRef } from "react";
+import Image from "next/image";
 
 export const Hero = () => {
-  const heroRef = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: heroRef,
-    offset: ["start end", "end start"],
-  });
-
-  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
-
-  useMotionValueEvent(translateY, "change", (latestValue) =>
-    console.log(latestValue)
-  );
-
   return (
-    <section
-      ref={heroRef}
-      className="pt-8 h-screen pb-20 md:pt-5 md:pb-10 bg-gradient-to-b  to-white from-[#D2DCFF] overflow-x-clip"
-    >
+    <section className="pt-8 h-screen pb-20 md:pt-5 md:pb-10 bg-gradient-to-b  to-white from-[#D2DCFF] overflow-x-clip">
       <div className="container">
         <div className="md:flex items-center">
           <div className="md:w-[478px]">
-            <div className="tag">Beta version is here</div>
+            <div className="tag">Comming soon....</div>
             <h1 className="text-5xl md:text-7xl font-bold tracking-tigher bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-6">
-              Pathway to productivity
+              AI Powered Chat Resume Builder
             </h1>
             <p className="text-xl text-[#010d3e] tracking-tight mt-6">
-              Celebrate the joy of accomplishment with an app designed to track
-              your progress, motivate your efforts, and celebrate your success.
+              Create a professional resume in minutes with our AI driven chat
+              based resume builder. Tailored for job seekers, powered by
+              advanced AI technology.
             </p>
             <div className="flex gap-1 items-center mt-[30px]">
               <button className="btn btn-primary">
@@ -54,40 +31,16 @@ export const Hero = () => {
           </div>
 
           <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
-            <motion.img
+            <Image
               src={cogImage.src}
               alt="Cog image"
               className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6 lg:left-0"
-              animate={{
-                translateY: [-20, 20],
-              }}
-              transition={{
-                repeat: Infinity,
-                repeatType: "mirror",
-                duration: 3,
-                ease: "easeInOut",
-              }}
+              height={1000}
+              width={1000}
+              blurDataURL="data:..."
+              placeholder="blur"
+              priority
             />
-            {/* <motion.img
-              src={cylinderImage.src}
-              alt="cylinder Image"
-              width={220}
-              height={220}
-              className="hidden md:block -top-8 -left-32 md:absolute"
-              style={{
-                translateY: translateY,
-              }}
-            />
-            <motion.img
-              src={noodleImage.src}
-              width={220}
-              alt="noodleImage"
-              className="hidden lg:block absolute top-[524px] left-[448px] rotate-[30deg]"
-              style={{
-                rotate: 30,
-                translateY: translateY,
-              }}
-            /> */}
           </div>
         </div>
       </div>
