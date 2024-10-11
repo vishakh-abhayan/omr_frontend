d9934bbc6917a872823c57912837deae60bc905b
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-10-11 19:56:52 +0530

    fix:add optimize image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main>
       <Header />
       <Hero />
-      <LogoTicker />
+      {/* <LogoTicker /> */}
       <ProductShowcase />
       {/* <Pricing /> */}
       {/* <Testimonials /> */}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import ArrowIcon from "@/assets/arrow-right.svg";
-import cogImage from "@/assets/cog.png";
-import cylinderImage from "@/assets/cylinder.png";
-import noodleImage from "@/assets/noodle.png";
-import Image from "next/image";
+import cogImage from "@/assets/Cog.webp";
+
 import {
   motion,
   useScroll,
